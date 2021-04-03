075b03921cbc907c8f7f87074372d38d37f08eb2
Azan20 <81761311+Azan20@users.noreply.github.com>
2021-04-03 16:50:09 +0100

    Add files via upload

---
+++ csc104ass.py
@@ -0,0 +1,32 @@
+'''write a short python code to display the name of you name and department'''
+name = "Adejare Yemi Hassan"
+department = "computer Science"
+print(name)
+print(department)
+'''assign the string "Boo!" to the variable scare and print the variable'''
+(scare) = "Boo!"
+print (scare)
+'''add two variables and assign the result to a third variable. print all the variable names.'''
+variable1 = 8
+variable2 = 5
+variable3 = (variable1 + variable2)
+print (variable3)
+'''divide 12 by the decimal .5 and assign the result to the variable outcome and print variable'''
+division = 12/.5
+outcome = (division)
+print (outcome)
+'''assign the sum of two numbers to a variable and print the variable'''
+a = 9
+b = 6
+c = (a + b)
+print (c)
+'''write the code that finds the reminder when 8 is divide by 3'''
+modulus1 = 8
+modulus2 = 3
+result = (modulus1 % modulus2)
+print (result)
+'''rewrite the following statement to force this order: first, multiply 2 by 4. then add 4 and 2. then multiply the first result by the second result. print the result'''
+a = 2 * 4
+b = 4 + 2
+c = (a * b)
+print (c)
